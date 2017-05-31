c0dc01e0d8b908a06c19eeea920407402b61717c
ehong <yihang.wang@tiberresources.com>
2017-05-31 16:30:58 +1000

    changed a version of chrome driver

---
+++ TelstraDefenceAutomation/ConsoleApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TelstraDefenceAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ".";
+            IWebDriver driver = new ChromeDriver(path);
+
+            driver.Navigate().GoToUrl("https://tcsportal.tollgroup.com/Account/Login");
+
+            IWebElement usernameField = driver.FindElement(By.Id("UserName"));
+            IWebElement passwordField = driver.FindElement(By.Id("Password"));
+            IWebElement loginBtn = driver.FindElement(By.TagName("button"));
+
+
+            usernameField.SendKeys("Shahanaz.Syzed@team.telstra.com");
+            passwordField.SendKeys("password123");
+            loginBtn.Click();
+
+
+
+        }
+    }
+}
